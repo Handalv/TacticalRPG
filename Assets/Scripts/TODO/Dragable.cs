393ca1c735e401020e651909641086d830024a1d
Sen <telderasil@mail.ru>
2019-10-19 17:22:12 +0300

    Units Panel

---
+++ Assets/Scripts/TODO/Dragable.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class Dragable : MonoBehaviour, IDragHandler, IBeginDragHandler, IEndDragHandler
+{
+    public void OnBeginDrag(PointerEventData eventData)
+    {
+
+    }
+
+    public void OnDrag(PointerEventData eventData)
+    {
+        this.transform.position = eventData.position;
+    }
+
+    public void OnEndDrag(PointerEventData eventData)
+    {
+
+    }
+}
